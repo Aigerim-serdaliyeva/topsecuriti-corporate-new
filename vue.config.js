292d7325e5d20bfb52e9907a0200821989279127
Sanzhar <sanch941@gmail.com>
2019-01-28 18:03:39 +0600

    success

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ const path = require("path");
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/sanch941.github.io/'
-    : '/',
+    : '/'
+    ,
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
